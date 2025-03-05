50abffddff6a9016e7daa79c765d0c9d05aaf904
l-efa <lefa.sahi@gmail.com>
2025-03-05 10:17:07 +0200

    render setup

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from "express";
+import cors from "cors";
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 let notes = [
   {
@@ -72,7 +75,7 @@ app.post("/api/notes", (request, response) => {
   response.json(note);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
