87728697340113b89bd57013116a2d1e9dcffa58
l-efa <lefa.sahi@gmail.com>
2025-03-05 12:15:33 +0200

    cors ja proxy

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 import express from "express";
-import cors from "cors";
 const app = express();
 
 app.use(express.json());
 app.use(express.static("dist"));
-app.use(cors());
 
 let notes = [
   {
